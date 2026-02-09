e74a16789e96eebf0cdfb059d7274bd8339ffe0f
osaze <osazezaboli@gmail.com>
2026-02-09 04:08:18 +0100

    milestone 3 fetch actresses con typeguard

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,50 @@ type Actress = Person & {
     | "South Korean"
     | "Chinese";
 };
+
+// typeguard di supporto per il fetch asyncrono sottostate
+function isActress(data: unknown): data is Actress {
+  if (
+    data &&
+    typeof data === "object" &&
+    "id" in data &&
+    typeof data.id === "number" &&
+    "name" in data &&
+    typeof data.name === "string" &&
+    "birth_year" in data &&
+    typeof data.birth_year === "number" &&
+    "death_year" in data &&
+    typeof data.death_year === "number" &&
+    "biography" in data &&
+    typeof data.biography === "string" &&
+    "image" in data &&
+    typeof data.image === "string" &&
+    "most_famous_movies" in data &&
+    Array.isArray(data.most_famous_movies) &&
+    "awards" in data &&
+    typeof data.awards === "string" &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const data = await response.json();
+    if (isActress(data)) {
+      return data;
+    } else throw new Error("la risposta non ha il formato giusto");
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+getActress(2).then((res) => console.log(res));
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Just some - more - TypeScript Snacks!</h1>
