e44854582ffda56f3257530c59451825032ecbd7
christianzaboli <zabolichristian@gmail.com>
2026-02-10 01:09:39 +0100

    sviluppo bonuses

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,18 @@ type Actress = Person & {
     | "Chinese";
 };
 
+type Actor = Person & {
+  known_for: [string, string, string];
+  awards: [string] | [string, string];
+  nationality:
+    | Actress["nationality"]
+    | "Scottish"
+    | "New Zealand"
+    | "Hong Kong"
+    | "German"
+    | "Canadian"
+    | "Irish";
+};
 // typeguard di supporto per il fetch asyncrono sottostate
 function isActress(data: unknown): data is Actress {
   return (
@@ -51,7 +63,37 @@ function isActress(data: unknown): data is Actress {
     typeof data.nationality === "string"
   );
 }
+function isActor(data: unknown): data is Actor {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "id" in data &&
+    typeof data.id === "number" &&
+    "name" in data &&
+    typeof data.name === "string" &&
+    "birth_year" in data &&
+    typeof data.birth_year === "number" &&
+    "death_year" in data &&
+    typeof data.death_year === "number" &&
+    "biography" in data &&
+    typeof data.biography === "string" &&
+    "image" in data &&
+    typeof data.image === "string" &&
+    "known_for" in data &&
+    data.known_for instanceof Array &&
+    data.known_for.length === 3 &&
+    data.known_for.every((e) => typeof e === "string") &&
+    "awards" in data &&
+    data.awards instanceof Array &&
+    (data.awards.length === 1 || data.awards.length === 2) &&
+    data.awards.every((e) => typeof e === "string") &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  );
+}
+
 // ONE FETCH
+// F
 async function getActress(id: number): Promise<Actress | null> {
   try {
     const response = await fetch(`http://localhost:3333/actresses/${id}`);
@@ -69,7 +111,27 @@ async function getActress(id: number): Promise<Actress | null> {
     return null;
   }
 }
-getActress(2).then((res) => console.log(res));
+getActress(2).then((res) => console.log("Risultato di getActress", res));
+
+// M
+async function getActor(id: number): Promise<Actor | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actors/${id}`);
+    const data = await response.json();
+    if (!isActor(data)) {
+      throw new Error("la risposta non ha il formato giusto");
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`errore nel recupero di id ${id}`, error);
+    } else {
+      console.error("errore sconosciuto", error);
+    }
+    return null;
+  }
+}
+getActor(1).then((res) => console.log("Risultato di getActor", res));
 
 // FULL FETCH
 async function getAllActresses(): Promise<Actress[]> {
@@ -107,6 +169,31 @@ async function getActresses(ids: number[]): Promise<(Actress | null)[]> {
 }
 getActresses([1, 2, 3]).then((res) => console.log(res));
 
+// 🎯 BONUS 1
+// Crea le funzioni:
+
+// createActress
+// updateActress
+// Utilizza gli Utility Types:
+
+// Omit: per creare un'attrice senza passare id, che verrà generato casualmente.
+// Partial: per permettere l’aggiornamento di qualsiasi proprietà tranne id e name.
+
+function createActress(data: Omit<Actress, "id">): Actress {
+  return {
+    id: Math.floor(Math.random() * 100),
+    ...data,
+  };
+}
+function updateActress(
+  actress: Actress,
+  updates: Partial<Omit<Actress, "id" | "name">>,
+): Actress {
+  return {
+    ...actress,
+    ...updates,
+  };
+}
 // html
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
